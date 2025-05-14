89df66b29be4d5ec48c7c6dbadd436c06dd414f4
sunghyuny <zeraxeon12@gmail.com>
2025-05-14 16:13:22 +0900

    0.0.0.0
    Start 2025.05.14

---
+++ pet_project/src/test/java/pit/pet_project/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pit.pet_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
