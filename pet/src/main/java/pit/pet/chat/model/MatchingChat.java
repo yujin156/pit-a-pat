b18ccc07874363d71e7df90cdb5e45b72cfe7a66
yujin156 <yujinsong8226@naver.com>
2025-05-19 12:26:17 +0900

    0.0.0.1
    프로필 엔티티 삭제, 키워드1, 키워드2 수정

--- pet/src/main/java/pit/pet/chat/model/MatchingChat.java
+++ pet/src/main/java/pit/pet/chat/model/MatchingChat.java
@@ -2,8 +2,9 @@ package pit.pet.chat.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+
+import org.springframework.context.annotation.Profile;
 import pit.pet.Account.User.Dog;
-import pit.pet.Account.User.Profile;
 
 import java.time.LocalDateTime;
 
@@ -27,9 +28,6 @@ public class MatchingChat {
     @JoinColumn(name = "receiver_dog_id")
     private Dog receiverDId;  // 받는 강아지
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "profile_id")
-    private Profile profile;
 
     @Column(name = "m_chat", nullable = false)
     private String mChat;
