3307ca070f6a35925abbc9f91588982a3c67f99a
bambeebeecoco <kyujongbo@gmail.com>
2025-05-22 17:37:53 +0900

    0.0.0.10
    friend.html 코드 수정
    friend_profile add chat icon

--- pet/src/main/resources/static/js/Friend_profile.js
+++ pet/src/main/resources/static/js/Friend_profile.js
@@ -159,12 +159,17 @@ function updateProfile() {
                     <span class="profile_type"></span>
                     <label class="profile_white">소형견</label>
                     <div class="profile_star" id="favorite-star">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="38.601" height="36.856" viewBox="0 0 38.601 36.856">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.601 36.856">
                             <g id="Artboard" transform="translate(1.5 1.5)">
                                 <g id="star">
                                     <path id="Shape" d="M17.8,0l5.5,11.143,12.3,1.8L26.7,21.61l2.1,12.247-11-5.785-11,5.785L8.9,21.61,0,12.941l12.3-1.8Z" fill="#B7B7B7" stroke="#B7B7B7" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" fill-rule="evenodd"/>
                                 </g>
                             </g>
+                        </svg>
+                    </div>
+                    <div class="profile_chat_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="chat">
+                            <path d="M8,11a1,1,0,1,0,1,1A1,1,0,0,0,8,11Zm4,0a1,1,0,1,0,1,1A1,1,0,0,0,12,11Zm4,0a1,1,0,1,0,1,1A1,1,0,0,0,16,11ZM12,2A10,10,0,0,0,2,12a9.89,9.89,0,0,0,2.26,6.33l-2,2a1,1,0,0,0-.21,1.09A1,1,0,0,0,3,22h9A10,10,0,0,0,12,2Zm0,18H5.41l.93-.93a1,1,0,0,0,.3-.71,1,1,0,0,0-.3-.7A8,8,0,1,1,12,20Z"></path>
                         </svg>
                     </div>
                 </div>
